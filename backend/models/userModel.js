125eff8c6b1fd91f6e89e776c76697387686a066
Ar4m <a-j.tosbath@orange.fr>
2022-03-20 15:26:23 +0100

    MongoDB + JWT + Cloudinary + Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pic: {
       type: String,
-      required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    hobbies: { type: Array, default: [], required: true }
+    hobbies: { type: Array, default: [] }
   },
-  { timestaps: true }
+  { timestamps: true }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
