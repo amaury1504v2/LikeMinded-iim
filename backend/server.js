eab7aed380a26e6a6392f8c754969a7a9c44ffab
Ar4m <a-j.tosbath@orange.fr>
2022-03-24 12:19:54 +0100

    search user and chat

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -16,7 +17,8 @@ app.get('/', (req, res) => {
     res.send("API is Running");
 });
 
-app.use('/api/user',userRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
